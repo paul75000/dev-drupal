04199fb3be4dbeebc592946633616079eef45777
Nam <n.truong.lt@gmail>
2018-06-05 16:12:34 +0200

    exo list 10 article + link to node

--- src/Controller/OngletController.php
+++ src/Controller/OngletController.php
@@ -5,7 +5,32 @@ namespace Drupal\hello\Controller;
 use Drupal\Core\Controller\ControllerBase;
 
 class OngletController extends ControllerBase{
-  public function content(){
-    return['#markup' => $this->t('page avec onglet')];
+  public function content($nodetype = NULL){
+  	//on construit notre requete
+    $query = $this->entityTypeManager()->getStorage('node')->getQuery();
+    // on met une condition au cas ou quelqu'un mettrais un param danns l'url
+    if($nodetype){
+    	$query->condition('type', $nodetype);
+    }
+    // on souhaite avoir un pager
+    $nids = $query->pager('10')->execute();
+
+    //on charge le resultat de la query
+    $nodes = $this->entityTypeManager()->getStorage('node')->loadMultiple($nids);
+
+    //on construit un tableau de liens vers les noeuds
+    $items = [];
+    foreach ($nodes as $node){
+    	$items[] = $node->toLink();
+    }
+
+    $list = [
+      '#theme' => 'item_list',
+      '#items' => $items,
+      '#title' => $this->t('node list title'),
+    ];
+
+    return $list;
+    //return['#markup' => /*$entities*/ $storage ];
   }
 }
